c5733621baa59af6002857ef10deff6b01611c79
Justin Mancherje <mancherje.justin@gmail.com>
2017-11-15 17:34:16 -0800

    WIP preparing for new directions state

--- src/containers/DirectionsContainer.js
+++ src/containers/DirectionsContainer.js
@@ -11,12 +11,18 @@ import {
 import {
   getSearchedRouteOptions,
   getActiveRouteIndex,
+  getCurrentSearchOrigin,
+  getCurrentSearchDestination,
 } from '../selectors/directions';
 
 import {
   updateActiveIndex,
   updateSearchedRouteOptions,
   resetActiveSearchedRoutes,
+  updateCurrentSearchOrigin,
+  updateCurrentSearchDestination,
+  clearCurrentSearchResultsOrigin,
+  clearCurrentSearchResultsDestination,
 } from '../actions/directions';
 
 import {
@@ -30,12 +36,17 @@ const mapStateToProps = (state) => ({
   currentLocation: getCurrentLocation(state),
   currentStepCount: getCurrentStepCount(state),
   activeRouteIndex: getActiveRouteIndex(state),
+  currentSearchOrigin: getCurrentSearchOrigin(state),
+  currentSearchDestination: getCurrentSearchDestination(state),
 });
-
 
 export default connect(mapStateToProps, {
   resetCurrentStepCount,
   updateActiveIndex,
   updateSearchedRouteOptions,
   resetActiveSearchedRoutes,
+  updateCurrentSearchOrigin,
+  updateCurrentSearchDestination,
+  clearCurrentSearchResultsOrigin,
+  clearCurrentSearchResultsDestination,
 })(Directions);
--- src/reducers/directions.js
+++ src/reducers/directions.js
@@ -1,4 +1,4 @@
-import { fromJS } from 'immutable';
+import { fromJS, Map } from 'immutable';
 
 import actionTypes from '../actions/actionTypes';
 
@@ -12,12 +12,42 @@ const initialStepsState = fromJS({
   // TODO Saved routes
 });
 
+const updateCurrentSearch = (state, { data, details }, originOrDestination) => {
+  const locationData = Map({
+    // Long description
+    description: data.description,
+    // Short description
+    name: details.name,
+    dataPlaceId: data.place_id,
+    detailsPlaceId: details.place_id,
+    coordinates: Map({
+      longitude: details.geometry.location.lat,
+      latitude: details.geometry.location.lng,
+    }),
+    viewPort: Map({
+      northEast: Map({
+        longitude: details.geometry.viewport.northeast.lat,
+        latitude: details.geometry.viewport.northeast.lng,
+      }),
+      southWest: Map({
+        longitude: details.geometry.viewport.southwest.lat,
+        latitude: details.geometry.viewport.southwest.lng,
+      }),
+    }),
+  });
+  return state.setIn(['currentSearch', originOrDestination], locationData);
+};
+
 export default (state = initialStepsState, { type, payload }) => {
   switch (type) {
   case actionTypes.directions.activeIndex.UPDATE:
     return state.set('activeRouteIndex', payload);
   case actionTypes.directions.searchedRouteOptions.UPDATE:
     return state.set('searchedRouteOptions', payload);
+  case actionTypes.directions.currentSearch.destination.UPDATE:
+    return updateCurrentSearch(state, payload, 'destination');
+  case actionTypes.directions.currentSearch.origin.UPDATE:
+    return updateCurrentSearch(state, payload, 'origin');
   default:
     return state;
   }
