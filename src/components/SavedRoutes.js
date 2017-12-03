1a16109d4b44b564dc190cf4683c8c2c7fc4641f
Justin Mancherje <mancherje.justin@gmail.com>
2017-12-02 16:26:43 -0800

    Drop Acordion and do Collapsible for SavedRoutes

--- src/components/SavedRoutes.js
+++ src/components/SavedRoutes.js
@@ -1,6 +1,7 @@
 import { List, Map } from 'immutable';
 import React from 'react';
 import { View } from 'react-native';
+import { identity } from 'lodash';
 import PropTypes from 'prop-types';
 import Collapsible from 'react-native-collapsible';
 import {
@@ -13,26 +14,34 @@ import {
   Grid,
   Col,
   Row,
+  CheckBox,
 } from 'native-base';
 
-import { metersToMiles } from '../helpers/conversions';
 import sharedStyles from './styles/sharedStyles';
 
 const STEPS_PER_METER = 0.713;
 
-class RouteHeader extends React.Component {
+class RouteDetails extends React.Component {
   static propTypes = {
     route: PropTypes.instanceOf(Map).isRequired,
+    isSelected: PropTypes.bool.isRequired,
+    toggleSelection: PropTypes.func.isRequired,
   };
+
   state = {
     active: false,
   };
+
   handlePress = () => {
     this.setState({ active: !this.state.active });
   };
+
+  handleCheckBoxPress = () => {
+    this.props.toggleSelection(this.props.route.get('_wId'));
+  };
+
   render() {
-    const { route } = this.props;
-    console.log('route', route.toJS());
+    const { route, isSelected } = this.props;
     return (
       <View>
         <ListItem
@@ -40,8 +49,16 @@ class RouteHeader extends React.Component {
           onPress={ this.handlePress }
         >
           <Grid>
-            <Row><Text>{ route.get('name') }</Text></Row>
-            <Row><Text>{ `${Math.round(STEPS_PER_METER * (route.getIn(['route', 'distance'])))} Steps` }</Text></Row>
+            <Col size={ 1 } style={ { justifyContent: 'center' } }>
+              <CheckBox
+                checked={ isSelected }
+                onPress={ this.handleCheckBoxPress }
+              />
+            </Col>
+            <Col size={ 6 }>
+              <Row><Text>{ route.get('name') }</Text></Row>
+              <Row><Text>{ `${Math.round(STEPS_PER_METER * (route.getIn(['route', 'distance'])))} Steps` }</Text></Row>
+            </Col>
           </Grid>
         </ListItem>
         <Collapsible collapsed={ !this.state.active }>
@@ -61,11 +78,26 @@ export default class SavedRoutes extends React.Component {
     clearAllSavedRoutes: PropTypes.func.isRequired,
   };
 
-  state = { activeIndex: null };
+  state = {
+    selectedRouteIds: List(),
+  };
 
-  activateIndex = (index) => {
-    const nextIndex = index === this.state.activeIndex ? null : index;
-    this.setState({ activeIndex: nextIndex });
+  toggleSelection = (id) => {
+    const { selectedRouteIds } = this.state;
+    if (selectedRouteIds.includes(id)) {
+      this.setState({ selectedRouteIds: selectedRouteIds.filterNot(routeId => routeId === id) });
+      return;
+    }
+    this.setState({ selectedRouteIds: selectedRouteIds.push(id) });
+  };
+
+  getTotalSteps = () => {
+    const { selectedRouteIds } = this.state;
+    const { savedRoutes } = this.props;
+
+    const selectedRoutes = selectedRouteIds.map(id => savedRoutes.find(route => route.get('_wId') === id)).filter(identity);
+    const totalDistance = selectedRoutes.reduce((distance, route) => distance + route.getIn(['route', 'distance']), 0);
+    return Math.round(totalDistance * STEPS_PER_METER);
   };
 
   render() {
@@ -76,20 +108,27 @@ export default class SavedRoutes extends React.Component {
         <Content>
           <NbList>
             { !savedRoutes.size ? placeholder : (
-              savedRoutes.map(route => (
-                <RouteHeader
-                  route={ route }
+              savedRoutes.map((route, index) => (
+                <RouteDetails
                   key={ route.get('_wId') }
+                  route={ route }
+                  isSelected={ this.state.selectedRouteIds.includes(route.get('_wId')) }
+                  toggleSelection={ this.toggleSelection }
                 />
               ))
             ) }
-            { savedRoutes.size ? (
-              <ListItem style={ sharedStyles.listStackCorrection }>
-                <Button transparent onPress={ this.props.clearAllSavedRoutes }>
-                  <Text>Clear all Saved Routes</Text>
-                </Button>
+            <Collapsible collapsed={ !this.state.selectedRouteIds.size }>
+              <ListItem>
+                <Text>
+                  { `${this.getTotalSteps()} Steps` }
+                </Text>
               </ListItem>
-            ) : null }
+            </Collapsible>
+            <ListItem style={ sharedStyles.listStackCorrection }>
+              <Button transparent onPress={ this.props.clearAllSavedRoutes } disabled={ !this.props.savedRoutes.size }>
+                <Text>Clear all Saved Routes</Text>
+              </Button>
+            </ListItem>
           </NbList>
         </Content>
       </Container>
